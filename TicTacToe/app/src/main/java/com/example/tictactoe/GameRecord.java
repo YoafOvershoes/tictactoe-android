c549608c5c77a9f5a3461431acf7fe4eb61ff919
Yoav Zinger <47221812y@gmail.com>
2022-05-14 11:32:37 +0300

    WIP, creating the room db

--- TicTacToe/app/src/main/java/com/example/tictactoe/GameRecord.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/GameRecord.java
@@ -4,7 +4,7 @@ import androidx.room.ColumnInfo;
 import androidx.room.Entity;
 import androidx.room.PrimaryKey;
 
-@Entity
+@Entity(tableName = "game_record")
 public class GameRecord {
     @PrimaryKey(autoGenerate = true)
     public int id;
--- TicTacToe/app/src/main/java/com/example/tictactoe/GameRecordDao.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/GameRecordDao.java
@@ -1,5 +1,6 @@
 package com.example.tictactoe;
 
+import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.Query;
@@ -8,8 +9,8 @@ import java.util.ArrayList;
 
 @Dao
 public interface GameRecordDao {
-    @Query("SELECT * FROM gamerecord ORDER BY record LIMIT 10")
-    ArrayList<GameRecord> getTopRecords();
+    @Query("SELECT * FROM game_record ORDER BY record LIMIT 10")
+    LiveData<ArrayList<GameRecord>> getTopRecords();
 
     @Insert
     void insertAll(GameRecord... records);
