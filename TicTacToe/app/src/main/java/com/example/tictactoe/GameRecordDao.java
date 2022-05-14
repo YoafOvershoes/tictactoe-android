0aebb73fc606d04dc5031c545776dbe7286e5bed
Yoav Zinger <47221812y@gmail.com>
2022-05-14 11:56:26 +0300

    wip db - created record repository and view model

--- TicTacToe/app/src/main/java/com/example/tictactoe/GameRecordDao.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/GameRecordDao.java
@@ -3,15 +3,20 @@ package com.example.tictactoe;
 import androidx.lifecycle.LiveData;
 import androidx.room.Dao;
 import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
 import androidx.room.Query;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @Dao
 public interface GameRecordDao {
     @Query("SELECT * FROM game_record ORDER BY record LIMIT 10")
-    LiveData<ArrayList<GameRecord>> getTopRecords();
+    LiveData<List<GameRecord>> getTopRecords();
 
-    @Insert
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
     void insertAll(GameRecord... records);
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(GameRecord record);
 }
