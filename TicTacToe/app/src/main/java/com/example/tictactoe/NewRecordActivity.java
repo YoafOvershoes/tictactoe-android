c208bede357b17c69df6b7a0b3344f76dc5bc5b7
Yoav Zinger <47221812y@gmail.com>
2022-05-15 02:39:28 +0300

    wip creating new record screen

--- TicTacToe/app/src/main/java/com/example/tictactoe/NewRecordActivity.java
+++ TicTacToe/app/src/main/java/com/example/tictactoe/NewRecordActivity.java
@@ -2,13 +2,36 @@ package com.example.tictactoe;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class NewRecordActivity extends AppCompatActivity {
+
+    public static final String EXTRA_REPLY = "com.example.android.game_record_database.REPLY";
+
+    private EditText mEditNameView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_record);
+
+        mEditNameView = findViewById(R.id.PlayerNameViewItem);
+
+        final Button button = findViewById(R.id.button_save);
+        button.setOnClickListener(view -> {
+            Intent replyIntent = new Intent();
+            if (TextUtils.isEmpty(mEditNameView.getText())) {
+                setResult(RESULT_CANCELED, replyIntent);
+            } else {
+                String word = mEditNameView.getText().toString();
+                replyIntent.putExtra(EXTRA_REPLY, word);
+                setResult(RESULT_OK, replyIntent);
+            }
+            finish();
+        });
     }
 }
